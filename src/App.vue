c29bfb4ccd57bc4a8374a933abd50dbbdef86321
Natalya <natasha-vd@yandex.ru>
2021-12-10 14:16:18 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Form />
+  </div>
+</template>
+
+<script>
+import Form from "./components/Form.vue";
+
+export default {
+  name: "App",
+  components: {
+    Form,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  padding: 24px;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #fafafa;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
